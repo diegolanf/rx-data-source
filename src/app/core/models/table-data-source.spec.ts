6679a9e67360dafd91abe6e3f69525b86c5fd63e
diegolanf <diego@langarica.me>
2022-12-01 17:36:02 +0100

    Create tests for pagination parameters

--- src/app/core/models/table-data-source.spec.ts
+++ src/app/core/models/table-data-source.spec.ts
@@ -1,28 +1,26 @@
+import { HttpParams } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { DataSource } from '@app/core/models/data-source.model';
 import { PaginationStrategy, SortDirection } from '@app/core/models/table-config.model';
 import { RxState } from '@rx-angular/state';
 import { RxActionFactory } from '@rx-angular/state/actions';
-import { Subject } from 'rxjs';
+import { Observable, of, Subject, tap } from 'rxjs';
+import { RunHelpers, TestScheduler } from 'rxjs/internal/testing/TestScheduler';
 
 import { TABLE_DATA_SOURCE_CONFIG, TableDataSource } from './table-data-source.model';
 import SpyObj = jasmine.SpyObj;
 
-interface TestTableData {
-  value: number;
-}
-
 describe('TableDataSource', () => {
-  let dataSourceSpy: SpyObj<DataSource<TestTableData[]>>;
-  let tableDataSource: TableDataSource<TestTableData>;
-  // let testScheduler: TestScheduler;
-  // const source: (params: HttpParams) => Observable<TestTableData[]> = (params: HttpParams) => {
-  //   const skip = +(params.get('skip') ?? 0);
-  //   const take = +(params.get('take') ?? 1);
-  //   return of([{ value: skip }, { value: skip + take }]);
-  // };
+  let dataSourceSpy: SpyObj<DataSource<number[]>>;
+  let tableDataSource: TableDataSource<number>;
+  let testScheduler: TestScheduler;
+  const _source: (params: HttpParams) => Observable<number[]> = (params: HttpParams) => {
+    const skip = +(params.get('skip') ?? 0);
+    const take = +(params.get('take') ?? 1);
+    return of([skip, skip + take]);
+  };
 
-  const data$ = new Subject<TestTableData[]>();
+  const data$ = new Subject<number[]>();
 
   beforeEach(() => {
     dataSourceSpy = jasmine.createSpyObj('DataSource', ['connectSource', 'refresh', 'reset'], {
@@ -50,16 +48,60 @@ describe('TableDataSource', () => {
       ],
     });
     tableDataSource = TestBed.inject(TableDataSource);
-    // testScheduler = new TestScheduler((actual: unknown, expected: unknown) => {
-    //   expect(actual).toEqual(expected);
-    // });
+    testScheduler = new TestScheduler((actual: unknown, expected: unknown) => {
+      expect(actual).toEqual(expected);
+    });
   });
 
   it('should create an instance', () => {
     expect(tableDataSource).toBeTruthy();
   });
 
-  it('should call connectSource from dataSource on initialisation', () => {
+  it('should call connectSource from dataSource during initialisation', () => {
     expect(dataSourceSpy.connectSource).toHaveBeenCalled();
+  });
+
+  // Pagination parameters
+  it('should return pagination parameters from provided configuration', () => {
+    const unsub = '-!';
+    const expectedMarbles = 'a';
+    const expectedValues = {
+      a: { skip: 30, take: 30 },
+    };
+
+    testScheduler.run(({ expectObservable }: RunHelpers) => {
+      expectObservable(tableDataSource.paginationParams$, unsub).toBe(
+        expectedMarbles,
+        expectedValues
+      );
+    });
+  });
+
+  it('should update pagination parameters when changing current page', () => {
+    const unsub = '---!';
+    const expectedMarbles = 'abc';
+    const expectedValues = {
+      a: { skip: 30, take: 30 },
+      b: { skip: 60, take: 30 },
+      c: { skip: 300, take: 30 },
+    };
+
+    const triggerMarbles = '-de';
+    const triggerValues = {
+      d: (): void => {
+        tableDataSource.nextPage();
+      },
+      e: (): void => {
+        tableDataSource.jumpToPage(11);
+      },
+    };
+
+    testScheduler.run(({ expectObservable, cold }: RunHelpers) => {
+      expectObservable(tableDataSource.paginationParams$, unsub).toBe(
+        expectedMarbles,
+        expectedValues
+      );
+      expectObservable(cold(triggerMarbles, triggerValues).pipe(tap((fn: () => void) => fn())));
+    });
   });
 });
