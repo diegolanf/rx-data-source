4b79bc704a2299c989444048683f69a5e3e769ac
diegolanf <diego@langarica.me>
2022-12-06 09:09:20 +0100

    Change map for tap on api service

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,23 +1,21 @@
 import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { generateArray } from '@app/shared/utils/generate.utils';
-import { delay, map, Observable, of } from 'rxjs';
+import { delay, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private totalRows = 25;
+  private readonly totalRows = 25;
 
   public getRows(params: HttpParams): Observable<number[]> {
     const skip = +(params.get('skip') ?? 0);
     const take = +(params.get('take') ?? this.totalRows);
-    console.info('Params - Skip:', skip, ', Take:', take);
     return of(generateArray((i: number) => skip + i + 1, take)).pipe(
       delay(2000),
-      map((r: number[]) => {
+      tap((r: number[]) => {
         console.info('API response:', r);
-        return r;
       })
     );
   }
